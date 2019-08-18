9d0bc2c2517b384565556fc8c7f6a5bc5f4a695b
Sean Tracey <seanmtracey@gmail.com>
2019-08-18 18:08:11 +0100

    Create code for viewing existing twins.

--- routes/view.js
+++ routes/view.js
@@ -2,12 +2,31 @@ const debug = require('debug')('routes:view');
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('view', { 
-		title: 'Digital Twin Viewer',
-		stylesheet : "view.css"
-	});
+const twins = require(`${__dirname}/../bin/lib/twins`);
+
+const UUIDRegex = `[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}`;
+
+router.get(`/:UUID(${UUIDRegex})`, function(req, res, next) {
+	twins.get(req.params.UUID)
+		.then(twin => {
+			
+			debug("gettwin:", twin[0]);
+			
+			twin[0].nodes = JSON.stringify(twin[0].nodes);
+
+			res.render('view', { 
+				title: 'Digital Twin Editor',
+				stylesheet : "view.css",
+				twinData : twin[0]
+			});
+
+		})
+		.catch(err => {
+			debug('Getting twin error:', err);
+			res.error = 500;
+			next();
+		})
+	;
 });
 
 module.exports = router;
